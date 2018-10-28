40533b9a51c60dee379292f847d4a371e3a4bb40
Maria Dees <mcdees2010@gmail.com>
2018-10-28 12:28:17 -0700

    created the login form

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import Layout from './components/common/Layout/Layout';
 import { Route, Switch } from 'react-router-dom';
 import Home from './components/Home/Home';
+import Login from './components/Login/Login';
 
 class App extends Component {
+  state = { currentUser: httpClient.getCurrentUser()}
+  onAuthSuccess = () => {
+    this.setState({currentUser: httpClient.getCurrentUser()});
+  }
   render() {
+    let { currentUser } = this.state;
+    let { onAuthSuccess } = this;
     return (
       <Layout>
           <Switch>
               <Route exact path="/" component={Home}/>
+              <Route path="/login" render={(props) => {
+                return <Login {...props} onLoginSuccess={onAuthSuccess}/>
+              }}/>
           </Switch>
       </Layout>
     )
