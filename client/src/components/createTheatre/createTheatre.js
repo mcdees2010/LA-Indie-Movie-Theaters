7e5b493bcb000feccb5eb959d5cf2eb75e6426bc
Maria Dees <mcdees2010@gmail.com>
2018-10-29 12:52:38 -0700

    made a create theatre file and started the state

--- client/src/components/createTheatre/createTheatre.js
+++ client/src/components/createTheatre/createTheatre.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class createTheatre extends Component{
+    state = {
+        name: "",
+        image: ""
+    }
     render(){
         return(
             <h1>create</h1>
