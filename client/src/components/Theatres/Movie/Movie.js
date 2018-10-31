1d6ad202f65c3b2af5f8464da8cac55e5d35c93f
Maria Dees <mcdees2010@gmail.com>
2018-10-30 22:15:51 -0700

    created the movie component and it works

--- client/src/components/Theatres/Movie/Movie.js
+++ client/src/components/Theatres/Movie/Movie.js
@@ -8,11 +8,23 @@ class Movie extends Component{
     async componentDidMount(){
         let { id } = this.props.match.params;
         let movieData = await axios.get(`https://api.internationalshowtimes.com/v4/movies/?cinema_id=${id}&apikey=RhuZxXz2vTqfvHw7sfhlcLt8UMevNdgw`);
-        console.log(movieData)
+        let movies = movieData.data.movies;
+        console.log(movies)
+        this.setState({ movies })
     }
     render(){
+        let { movies } = this.state;
         return(
-            <h1>hello</h1>
+            <div>
+                {movies.map((m, i) =>
+                    <ul>
+                        <li key={i}>
+                            {m.title}
+                        </li>
+                        <img src={m.poster_image_thumbnail}/>
+                    </ul>
+                )}
+            </div>
         )
     }
 }
