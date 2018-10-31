05aae11f0d8de7e7c36dbfe0b014a6acc34a234f
Maria Dees <mcdees2010@gmail.com>
2018-10-31 09:04:43 -0700

    adding showtime api

--- client/src/components/Theatres/Movie/Movie.js
+++ client/src/components/Theatres/Movie/Movie.js
@@ -5,11 +5,17 @@ class Movie extends Component{
     state = {
         movies: []
     }
+    constructMovie({title, id, poster_image_thumbnail}){
+        let movie = { title, id, poster_image_thumbnail, showtimes: []};
+        return movie;
+    }
     async componentDidMount(){
         let { id } = this.props.match.params;
         let movieData = await axios.get(`https://api.internationalshowtimes.com/v4/movies/?cinema_id=${id}&apikey=RhuZxXz2vTqfvHw7sfhlcLt8UMevNdgw`);
-        let movies = movieData.data.movies;
-        console.log(movies)
+        let showtimeData = await axios.get('https://api.internationalshowtimes.com/v4/showtimes/?cinema_id=40216&apikey=RhuZxXz2vTqfvHw7sfhlcLt8UMevNdgw');
+        console.log(showtimeData)
+        let { movies } = movieData.data;
+        
         this.setState({ movies })
     }
     render(){
