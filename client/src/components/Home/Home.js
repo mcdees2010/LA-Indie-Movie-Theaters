98423d51e2be6cc7afcddabf11e87e7643441bb1
Maria Dees <mcdees2010@gmail.com>
2018-11-01 09:33:35 -0700

    changed splash page picture

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends Component {
     render() {
         return (
             <div className="pic">
-                <h1>Welcome!</h1>
+                <h1>Showtimes LA</h1>
                 <div>
                 </div>
             </div>
