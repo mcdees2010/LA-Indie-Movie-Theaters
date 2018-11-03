77d3d66473cec95652694d486c5497ad9af0b271
Maria Dees <mcdees2010@gmail.com>
2018-11-02 17:30:23 -0700

    able to edit the favorite in postman

--- controllers/Favorite.js
+++ controllers/Favorite.js
@@ -5,13 +5,13 @@ exports.create = (req, res) => {
     User.findById(id, (err, user) => {
         if (err) res.json({success: false, err});
 
-        const found = user.favorites.filter(m => {
-            if (m.movieID === req.body.movieID) return m
-        });
+        // const found = user.favorites.filter(m => {
+        //     if (m.movieID === req.body.movieID) return m
+        // });
 
-        if (found[0]) {
-            res.json({ success: false, message: "Already favorited" })
-        }
+        // if (found[0]) {
+        //     res.json({ success: false, message: "Already favorited" })
+        // }
 
         user.favorites.push({...req.body});
         user.save((err, user) => {
@@ -22,21 +22,40 @@ exports.create = (req, res) => {
 }
 
 // Add Update Action
+// exports.update = (req, res) => {
+//     let { seen, wantToSee } = req.body;
+//     let { favorite_id } = req.params;
+//     // res.json({ user: req.user.id })
+//     User.findByIdAndUpdate(req.user.id, (err, user)=> {
+//         res.json({ user })
+//         if (err) res.json({success: false, err});
+//         let movie = user.favorites.id(favorite_id);
+//         if (!movie) {
+//             res.json({ success: false, message: "Movie not found" });
+//         };
+//         movie.seen = seen;
+//         movie.wantToSee = wantToSee;
+//         user.save((err, user) => {
+//             res.json({ success: true, user })
+//         })
+//     })
+// }
+
 exports.update = (req, res) => {
-    let { seen, wantToSee } = req.body;
     let { favorite_id } = req.params;
-    // res.json({ user: req.user.id })
-    User.findByIdAndUpdate(req.user.id, (err, user)=> {
-        res.json({ user })
+    let { seen, wantToSee } = req.body;
+    let { id } = req.user;
+    User.findByIdAndUpdate(id, {new: true}, (err, user) => {
         if (err) res.json({success: false, err});
         let movie = user.favorites.id(favorite_id);
-        if (!movie) {
-            res.json({ success: false, message: "Movie not found" });
-        };
+        if(!movie){
+            res.json({success: false, message: "Movie not found"});
+        }
         movie.seen = seen;
         movie.wantToSee = wantToSee;
         user.save((err, user) => {
-            res.json({ success: true, user })
+            if (err) res.json({success: false, err});
+            res.json({success: true, favorites: user.favorites})
         })
     })
 }
